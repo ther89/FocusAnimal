0c6f11797c59af07e5928bcc49c057d4c7e39bc3
ther89 <rebel89@gmail.com>
2021-03-17 12:54:49 +0100

    Initial files

---
+++ warningmodal.js
@@ -0,0 +1,31 @@
+var $myModal = $(document.createElement('div'))
+	.addClass("modal")
+	.attr('id', 'myModal');
+  
+$('body').append($myModal);
+
+$('#myModal').append($('<div class="modal-content"><span class="close">&times;</span><p>Biztos?</p></div>'));
+
+$('#myModal').show();
+
+// Get the modal
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  //modal.style.display = "none";
+  $('#myModal').hide();
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+	 $('#myModal').hide();
+    //modal.style.display = "none";
+  }
+}
+
+setTimeout(function() {
+	$('#myModal').show();
+}, 10000)
