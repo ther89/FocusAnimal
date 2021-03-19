0672a58291a17fe92182f0fa624bcc2ef5c2f537
ther89 <rebel89@gmail.com>
2021-03-19 09:18:07 +0100

    Image references and options added

--- options.js
+++ options.js
@@ -4,10 +4,13 @@ focusanimal.save_options = function() {
     var el_entries = document.getElementById('focusanimal_allowed_urls');
     var entries = el_entries.value;
     var enabled = document.getElementById('focusanimal_enabled').checked;
+    //var imageFileName = document.getElementById('focusanimal_image').value;
+    var imageFileName = document.querySelector('input[name="focusanimal_image"]:checked').value;
 
     chrome.storage.local.set({ 
         focusanimal_ALLOWED_URLS: entries.split('\n'),
         focusanimal_ENABLED: enabled,
+        focusanimal_IMAGE: imageFileName,
     }, function(){
         // Update status to let user know options were saved.
         var status = document.getElementById('status');
@@ -22,10 +25,13 @@ focusanimal.load_options = function() {
     chrome.storage.local.get({
         focusanimal_ALLOWED_URLS: '',
         focusanimal_ENABLED: false,
+        focusanimal_IMAGE: 'dog.jpg',
     }, function (result) {
 		//alert(JSON.stringify(result));
         document.getElementById('focusanimal_allowed_urls').value = result.focusanimal_ALLOWED_URLS.join('\n');
         document.getElementById('focusanimal_enabled').checked = result.focusanimal_ENABLED;
+        //document.getElementById('focusanimal_image').value = result.focusanimal_IMAGE;
+        var selected = document.querySelector('input[name="focusanimal_image"][value="'+ result.focusanimal_IMAGE  +'"]').checked = true;
     });
 };
 
--- warningmodal.js
+++ warningmodal.js
@@ -1,10 +1,12 @@
 chrome.storage.local.get({
   focusanimal_ALLOWED_URLS: '',
   focusanimal_ENABLED: false,
+  focusanimal_IMAGE: 'dog.jpg',
 }, function (result) {
   //alert(JSON.stringify(result));
   var urls = result.focusanimal_ALLOWED_URLS;
   var enabled = result.focusanimal_ENABLED;
+  var imageFileName = result.focusanimal_IMAGE;
 
   if(!enabled) return;
 
@@ -18,15 +20,23 @@ chrome.storage.local.get({
        break;
     }
   }
-  
+
   if(!matched) return;
 
   var $myModal = $(document.createElement('div'))
     .addClass("modal")
     .attr('id', 'myModal');
   $('body').append($myModal);
-  $('#myModal').append($('<div class="modal-content"><span class="close">&times;</span><p>Biztos?</p></div>'));
+  $('#myModal').append($('<div class="modal-content">'+
+  '<span class="close">&times;</span><p>Biztos?</p>'+
+  '<img id="focusanimal_image_display" /></div>'));
+  
+  var imgURL = chrome.extension.getURL('images/' + imageFileName);
+  document.getElementById("focusanimal_image_display").src = imgURL;
+
+  
   $('#myModal').show();
+
   // Get the modal
   var modal = document.getElementById("myModal");
   var span = document.getElementsByClassName("close")[0];
